434fda94f89c84f42df87fc43dfdde19a9fe2973
AgathaKarenne32 <agathakarenne32@gmail.com>
2025-11-26 16:38:15 +0000

    fix: corrige localizacao do WebConfig e limpa estrutura

---
+++ backend/src/main/java/com/callquality/api/config/WebConfig.java
@@ -0,0 +1,16 @@
+package com.callquality.api.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // Libera tudo
+                .allowedOrigins("*") // ATENÇÃO: Em produção, troque "*" pelo domínio do seu site
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
+    }
+}
